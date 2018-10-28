de65fd8cdacf6098ab4da9db80e9154250921446
Nathalie Santana <nathalie.scruz@gmail.com>
2018-10-28 03:10:37 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.name.artists
+    self.artists.collect {|a| a.name}
   end
 end
